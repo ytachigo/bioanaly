e1b20bec6933f420764a078dc09074780ee6d981
ytachigo <tachi.yuhei@gmail.com>
2018-07-19 19:04:31 +0900

    add a new function

--- func.py
+++ func.py
@@ -3,3 +3,7 @@ def floor(value, dignum):
 
 def ceil(value, dignum):
     return (int(value / dignum) + 1 ) * dignum
+
+def crosscorr(vec0, vec1):
+    value = np.dot(vec0, vec1) / (np.linalg.norm(vec0) * np.linalg.norm(vec1))
+    return value
