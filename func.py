69e5f48275d95b48161cf3b669da9a5bd1a1f369
ytachigo <tachi.yuhei@gmail.com>
2018-07-20 01:01:40 +0900

    update

--- func.py
+++ func.py
@@ -1,3 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.colors import LinearSegmentedColormap
+import seaborn as sns
+
 def floor(value, dignum):
     return int(value / dignum) * dignum
 
