1165a3180268c888061ac94c333a5e2b3f99947a
ytachigo <tachi.yuhei@gmail.com>
2018-12-13 01:26:08 +0900

    fixed

--- estimate.py
+++ estimate.py
@@ -7,16 +7,16 @@ import seaborn as sns
 
 def jackknife(series, n):
     cutsample = [samples for samples in zip(*[iter(series.tolist())]*n)]
-    Nsp = len(cutsample)
+    nsp = len(cutsample)
     av_list = []
 
-    for i in range(0, Nsp): 
+    for i in range(0, nsp): 
         resample = []
-        for j in range(0, Nsp):
+        for j in range(0, nsp):
             if j != i: resample += cutsample[i]
         av_list.append(np.mean(resample))
 
     jav = np.mean(av_list)
     delta = [(jav - av) * (jav - av) for av in av_list]
-    jerr = np.sqrt(np.sum(delta) / (Nsp - 1)) / np.sqrt(Nsp)
+    jerr = np.sqrt(np.sum(delta) / (nsp - 1)) / np.sqrt(nsp)
     return jav, jerr
